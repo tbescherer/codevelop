123cc7bb56321a64c4afe03791264948f2d69817
Tom Bescherer <Tbescherer@gmail.com>
2015-07-14 17:12:57 -0400

    Implement rudimentary user show view.

--- app/assets/javascripts/backbone/collections/users.js
+++ app/assets/javascripts/backbone/collections/users.js
@@ -1,5 +1,21 @@
 Codevelop.Collections.Users = Backbone.Collection.extend({
   url: "/users/",
+
+  getOrFetch: function(id) {
+    var user = this.get(id);
+    var users = this;
+    if (!user) {
+      user = new Codevelop.Models.User({id: id})
+      user.fetch({
+        success: function() {
+          users.add(user);
+        }
+      })
+    } else {
+      user.fetch();
+    }
+    return user;
+  }
 })
 
 Codevelop.Collections.users = new Codevelop.Collections.Users()
--- app/assets/javascripts/backbone/routers/router.js
+++ app/assets/javascripts/backbone/routers/router.js
@@ -1,12 +1,14 @@
 Codevelop.Routers.Router = Backbone.Router.extend({
 
   initialize: function(options){
-    this.$rootEl = options.$rootEl
-    this.users = options.users
+    this.$rootEl = options.$rootEl;
+    this.users = options.users;
   },
 
   routes: {
     '': 'index',
+    'users/:id': 'userShow',
+    'profile': "userProfile",
   },
 
   index: function() {
@@ -14,10 +16,18 @@ Codevelop.Routers.Router = Backbone.Router.extend({
     var view = new Codevelop.Views.UsersIndex({
       collection: this.users
     })
-    console.log("index called");
     this._swapView(view);
   },
 
+  userShow: function(id) {
+    var user = this.users.getOrFetch(id)
+    console.log("hello from usershow!")
+    var view = new Codevelop.Views.UserShow({model: user})
+    this._swapView(view);
+  },
+
+  userProfile: function() {
+  },
 
   _swapView: function(view) {
     this._currentView && this._currentView.remove();
