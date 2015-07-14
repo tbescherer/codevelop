7b1d81bb4b7d629342a9431751edeee13f6a96bb
Tom Bescherer <Tbescherer@gmail.com>
2015-07-14 11:02:42 -0400

    Give users controller new and create methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in(@user)
+      render json: @user
+    else
+      flash.new[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end
