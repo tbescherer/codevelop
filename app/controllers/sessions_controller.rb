cb1b6f4e8a55a131f1d405515bca19125aec38bd
Tom Bescherer <Tbescherer@gmail.com>
2015-07-14 11:05:08 -0400

    Give sessions controller new create and destory methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      sign_in(user)
+      render json: @user
+    else
+      flash.now[:errors] = ["Invalid username or password"]
+      render :new
+    end
   end
 
   def destroy
+    sign_out
+    redirect_to new_session_url
   end
 end
