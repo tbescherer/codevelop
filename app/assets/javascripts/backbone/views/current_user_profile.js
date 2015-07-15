f0fab571793677dcdea56c7023bbec38df4f3fa9
Thomas Bescherer <Tbescherer@gmail.com>
2015-07-15 09:56:34 -0400

    Import App Academy CompositeView Util.

--- app/assets/javascripts/backbone/views/current_user_profile.js
+++ app/assets/javascripts/backbone/views/current_user_profile.js
@@ -1,6 +1,11 @@
 Codevelop.Views.CurrentUserProfile = Backbone.View.extend({
   template: JST['current_user/show'],
 
+  events: {
+    "click #about-button": "aboutView",
+    "click #question-button": "questionView"
+  },
+
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
   },
@@ -9,5 +14,15 @@ Codevelop.Views.CurrentUserProfile = Backbone.View.extend({
     var content = this.template({user: this.model});
     this.$el.html(content);
     return this;
+  },
+
+  aboutView: function() {
+    console.log("hello from aboutview");
+    this.$el.find(".content-tabs").append("about")
+  },
+
+  questionView: function() {
+    console.log("hello from questionView")
+    this.$el.find(".content-tabs").append("question")
   }
 })
