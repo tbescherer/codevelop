d8085c02525cefd1204fe61ec554c6d0d1016f12
Tbescherer <tbescherer@gmail.com>
2015-07-17 17:10:57 -0400

    Working on editing profile still

--- app/assets/javascripts/backbone/views/about_view.js
+++ app/assets/javascripts/backbone/views/about_view.js
@@ -1,9 +1,17 @@
 Codevelop.Views.AboutView = Backbone.View.extend({
   template: JST['current_user/about'],
 
+  events: {
+    "click #change-self-summary #change-skills #change-message-if": 'editSummary'
+  },
+
   render: function() {
     var content = this.template({user: this.model});
     this.$el.html(content);
     return this;
+  },
+
+  editSummary: function(event){
+    $(event.currentTarget).html("hello!")
   }
 })
