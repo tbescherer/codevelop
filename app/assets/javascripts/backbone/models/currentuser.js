94054b34efc038774ffe1a418db12db1a057bad9
Tom Bescherer <Tbescherer@gmail.com>
2015-07-18 21:18:43 -0400

    give currentuser.js the saveFormData method.

--- app/assets/javascripts/backbone/models/currentuser.js
+++ app/assets/javascripts/backbone/models/currentuser.js
@@ -1,3 +1,23 @@
 Codevelop.Models.CurrentUser = Codevelop.Models.User.extend({
-  urlRoot: '/api/current_user'
+  urlRoot: '/api/current_user',
+
+  saveFormData: function(formData, options){
+    var model = this;
+
+    $.ajax({
+      url: _.result(model, "url"),
+      type: "PUT",
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(resp){
+        model.set(model.parse(resp));
+        model.trigger('sync', model, resp, options);
+        options.success && options.success(model, resp, options);
+      },
+      error: function(resp) {
+        options.error && options.error(model, resp, options);
+      }
+    });
+  }
 });
