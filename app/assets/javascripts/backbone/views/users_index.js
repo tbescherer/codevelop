143d1c8bb1e0d908134e6aca02f168c8c4a69f98
App Academy Student <appacademy@Apps-Mac-mini-14.local>
2015-07-22 09:48:30 -0400

    Give usersindex the search action.

--- app/assets/javascripts/backbone/views/users_index.js
+++ app/assets/javascripts/backbone/views/users_index.js
@@ -1,14 +1,31 @@
 Codevelop.Views.UsersIndex = Backbone.View.extend({
   template: JST['users/index'],
 
+  events: {
+    "change .query": "search"
+  },
+
   initialize: function(options) {
     this.listenTo(this.collection, 'sync', this.render),
     this.currentUser = options.currentUser
+    this.collection.pageNum = 1;
   },
 
   render: function() {
     var content = this.template({users: this.collection, currentUser: this.currenUser});
     this.$el.html(content)
     return this;
+  },
+
+  search: function(event) {
+    event.preventDefault();
+    this.collection.pageNum = 1;
+    this.collection.query = this.$(".query").val();
+    this.users.fetch({
+      data: {
+        query: this.collection.query,
+        page: this.collection.pageNum
+      }
+    });
   }
 })
