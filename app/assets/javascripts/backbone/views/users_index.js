509ab3e1d3726e837eda9a1ef20f4c72da707f63
App Academy Student <appacademy@Apps-Mac-mini-26.local>
2015-07-24 09:50:48 -0400

    Scrolling seems to work

--- app/assets/javascripts/backbone/views/users_index.js
+++ app/assets/javascripts/backbone/views/users_index.js
@@ -38,7 +38,7 @@ Codevelop.Views.UsersIndex = Backbone.View.extend({
 
   nextPage: function (event) {
     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-      if (this.collection.pageNum < this.collection.totalPages) {
+      if (this.collection.pageNum <= this.collection.totalPages) {
         $("#loading-message").html('<p>Loading more matches...</p>')
         this.collection.fetch({
           data: {
